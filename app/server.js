3e32384f1378fe055f68c2faa3c202080ee54595
Jezz Santos <jezz.santos@hotmail.com>
2017-09-04 21:53:40 +1200

    added files to make app run in Azure

--- app/server.js
+++ app/server.js
@@ -1,4 +1,4 @@
 var express = require('express');
 var server = express();
-server.use('/', express.static(__dirname + '/app'));
+server.use('/', express.static(__dirname + '/'));
 server.listen(80);
